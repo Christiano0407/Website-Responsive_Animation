fe47a971015616166758f4983fd6b08302c917f4
Christiano0407 <placebo0704@gmail.com>
2021-04-09 12:47:03 -0500

    menus

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
-const menuHamburger = document.getElementById(`mobile-menu`); 
+const menu = document.getElementById(`mobile-menu`); 
+const menuLinks = document.querySelector(`.navBar__menu`); 
+const navLogo = document.getElementById(`navBar-Logo`); 
+const body = document.querySelector(`body`); 
+         /* Display Mobile Menu */
+
+const mobileMenu = () => {
+    menu.classList.toggle(`is-active`); 
+    menuLinks.classList.toggle(`active`); 
+    body.classList.toggle(`active`); 
+}
+
+menu.addEventListener(`click`, mobileMenu); 
